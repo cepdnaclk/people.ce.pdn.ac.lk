5348407c2925039ebd36891fdde1ffe8e3bd4772
IshanFernando <e18098@eng.pdn.ac.lk>
2021-12-15 15:01:07 +0530

    Add interests | python script to fix issue47 - Adds interests frontmatter to profile pages

---
+++ python_scripts/add_interests_issue47.py
@@ -0,0 +1,31 @@
+# Inserts interests into profile pages to solve issue47
+# https://github.com/cepdnaclk/people.ce.pdn.ac.lk/issues/47
+
+
+import os
+
+filesPath = "../pages/students/e18/"
+
+
+allFiles = os.listdir(filesPath)
+for each in allFiles:
+    filePath = filesPath + each
+    thisFile = open(filePath, "r")
+    fileContents = thisFile.read()
+    thisFile.close()
+    splitted = fileContents.split("\n")
+
+    newArray = []
+    for eachLine in splitted:
+        if "projects_done" not in eachLine:
+            newArray.append(eachLine)
+            continue
+
+        newArray.append("interests: #")
+        newArray.append(eachLine)
+
+    newContent = "\n".join(newArray)
+
+    thisFile = open(filePath, "w")
+    thisFile.write(newContent)
+    thisFile.close()
