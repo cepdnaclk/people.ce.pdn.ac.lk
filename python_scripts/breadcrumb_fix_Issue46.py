7b94144cee33afdb8e81f9287a43b82d59bbc1b2
IshanFernando <e18098@eng.pdn.ac.lk>
2021-12-15 14:44:27 +0530

    Add python script to fix #46

---
+++ python_scripts/breadcrumb_fix_Issue46.py
@@ -0,0 +1,33 @@
+# Adds "/" at the end of permalink to solve issue46
+# https://github.com/cepdnaclk/people.ce.pdn.ac.lk/issues/46
+
+
+import os
+
+filesPath = "../pages/students/e15/"
+
+
+allFiles = os.listdir(filesPath)
+for each in allFiles:
+    filePath = filesPath + each
+    thisFile = open(filePath, "r")
+    fileContents = thisFile.read()
+    thisFile.close()
+    splitted = fileContents.split("\n")
+
+    newArray = []
+    for eachLine in splitted:
+        if "permalink" not in eachLine:
+            newArray.append(eachLine)
+            continue
+
+        if eachLine[-2] == "/":
+            print("/ is already there")
+            exit()
+        eachLine = eachLine[0:-1] + "/" + eachLine[-1]
+        newArray.append(eachLine)
+
+    newContent = "\n".join(newArray)
+
+    thisFile = open(filePath, "w")
+    thisFile.write(newContent)
