5fbeea43910672447e32c2f823f50b8bb6552071
IshanFernando <e18098@eng.pdn.ac.lk>
2022-01-11 21:42:07 +0530

    Add current affiliation to alumni CSV

---
+++ python_scripts/add_current_aff_alumni.py
@@ -0,0 +1,13 @@
+import json
+for x in range(2, 14):
+    jsonLink = f"../_data/stud/e{x:02}.json"
+    jsonData = dict(json.load(open(jsonLink)))
+
+    newJSON = {}
+    for each in jsonData:
+        newdata = jsonData[each]
+        newdata['current_affliation'] = "#"
+        newJSON[each] = newdata
+
+    writeFile = open(jsonLink, "w")
+    writeFile.write(json.dumps(newJSON, indent=4))
