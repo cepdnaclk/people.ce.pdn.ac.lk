cb74117f86e5267b253fc26b8b4ceab3a203a6bf
IshanFernando <e18098@eng.pdn.ac.lk>
2022-01-26 07:24:59 +0530

    Remove current_affiliation from alumni json

--- python_scripts/not_used/add_current_aff_alumni.py
+++ python_scripts/not_used/add_current_aff_alumni.py
@@ -1,13 +1,17 @@
-## Author: E/18/098 Ishan Fernando - e18098@eng.pdn.ac.lk
+# Author: E/18/098 Ishan Fernando - e18098@eng.pdn.ac.lk
 import json
 for x in range(2, 14):
-    jsonLink = f"../_data/stud/e{x:02}.json"
+    jsonLink = f"../../_data/stud/e{x:02}.json"
     jsonData = dict(json.load(open(jsonLink)))
 
     newJSON = {}
     for each in jsonData:
         newdata = jsonData[each]
-        newdata['current_affliation'] = "#"
+        # edited this to remove the curent aff line
+        try:
+            newdata.pop("current_affliation")
+        except:
+            print("current aff not in file")
         newJSON[each] = newdata
 
     writeFile = open(jsonLink, "w")
