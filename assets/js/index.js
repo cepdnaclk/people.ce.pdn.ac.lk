11a6a5ecee73bde798ba8ccf1507039d18dddcd6
NuwanJ <nuwanjaliyagoda@gmail.com>
2022-04-28 00:57:49 +0530

    Badges - part 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,3 +18,15 @@ $(document).ready(function () {
     return false;
   });
 });
+
+// // For Tool Tips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle-tooltip="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+//
+// // Popover
+// var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+//   return new bootstrap.Popover(popoverTriggerEl)
+// })
---
+++ python_scripts/update_badges.py
@@ -0,0 +1,8 @@
+import requests
+import os
+import json
+
+# Read the /batch folder and generate the file, _data/badges.json
+
+
+# Create pages for each badge
