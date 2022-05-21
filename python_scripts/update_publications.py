eb41b01ca3e932b1006f6da7c6c3788b8bd7c3f4
NuwanJ <nuwanjaliyagoda@gmail.com>
2022-05-22 00:36:52 +0530

    Staff research: grouped by years

--- python_scripts/update_publications.py
+++ python_scripts/update_publications.py
@@ -22,20 +22,38 @@ except:
     print('parse failed; ' +  url)
 
 # Get staff publications
-try:
-    url = "https://api.ce.pdn.ac.lk/publications/v1/filter/staff/index.json"
-    r = requests.get(url)
+# try:
+url = "https://api.ce.pdn.ac.lk/publications/v1/filter/staff/index.json"
+r = requests.get(url)
 
-    if r.status_code==200:
-        # it is available
-        staff_pub = json.loads(r.text)
-        filename = "../_data/staff_publications.json"
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        with open(filename, "w") as f:
-            f.write(json.dumps(staff_pub, indent = 4))
-        print("Staff: Success")
-    else:
-        print("Staff: Failed")
+if r.status_code==200:
+    # it is available
+    staff_pub = json.loads(r.text)
+    staff_pub_year = {}
 
-except:
-    print('parse failed; ' +  url)
+    # pre-process the data
+    for staff in staff_pub:
+        # print(staff_pub[staff])
+        pub_count = 0
+        if staff not in staff_pub_year: staff_pub_year[staff]= {"count": 0, "publications": {}}
+
+        for pub in staff_pub[staff]:
+            pub_count += 1
+            year = pub['year']
+
+            if year not in staff_pub_year[staff]["publications"]: staff_pub_year[staff]["publications"][year] = []
+            staff_pub_year[staff]["publications"][year].append(pub)
+
+        staff_pub_year[staff]['count'] = pub_count
+
+    filename = "../_data/staff_publications.json"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, "w") as f:
+        # f.write(json.dumps(staff_pub, indent = 4))
+        f.write(json.dumps(staff_pub_year, indent = 4))
+    print("Staff: Success")
+else:
+    print("Staff: Failed")
+
+# except:
+#     print('parse failed; ' +  url)
