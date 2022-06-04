bbeadf92b95f5aa3f32d9d84e7482a0d9d10149b
Nuwan Jaliyagoda <NuwanJ@users.noreply.github.com>
2022-06-04 20:49:49 +0530

    Postgraduate updates (#144)
    * Badges - part 1
    
    * Badge pages implementations
    
    * Badges - part 2
    
    * A few badge related updates
    
    * Delete CNAME
    
    * Updates
    
    * link correction
    
    * A few GUI updates
    
    * Updates
    
    * postgrad details layout
    
    * python script to donwload from google form
    
    * run python script
    
    * add postgrad index page
    
    * add link to navbar and home
    
    * Add new python script to github actions
    
    * Add link to google form
    
    * Hide the reg number in index list if 'provisonal'
    
    * Update temporary-academic.html
    
    * add new frontmatter var to detect which is latest addition
    
    * Move the card into _includes
    
    * add link to postgrad in breadcrumb
    
    * add some data and test
    
    * comments
    
    * improve the _includes to be cleaner
    
    * rename
    
    * comments
    
    * removed duplicate pages
    
    Co-authored-by: IshanFernando <e18098@eng.pdn.ac.lk>
    Co-authored-by: IshanFernando <73381996+ishanfdo18098@users.noreply.github.com>

--- python_scripts/update_postgrad_from_google_form.py
+++ python_scripts/update_postgrad_from_google_form.py
@@ -8,15 +8,14 @@
 import requests
 import os
 import gdown  # pip install gdown
-import json  # to edit _data/exx.json
-import student_profile_page_titles
-import resize_student_images
+import datetime
 
 googleFromCSV_link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR6ntuDUUuzQS84Am7NMcCc6LCTYKmUfMbVhp4dvy1AXWoVxNrjWfeQntWg5cAfLsFvb4WASQRp-erT/pub?output=csv"
 googleFromCSV = requests.get(googleFromCSV_link, headers={
                              'Cache-Control': 'no-cache'}).text.split("\n")
 
 # Index of the CSV parameters
+FORM_FILLED_DATE = 0
 EMAIL = 1
 REG_NO = 2
 NAME_WITH_INITIALS = 5
@@ -33,26 +32,20 @@ if __name__ == "__main__":
     for eachLine in googleFromCSV:
         studentData = eachLine.replace('\r', '').split(",")
 
-        # URL to website is optional therefore it may not be included in this line (last comma is not there)
-        # if len(studentData) != 16 :
-        #     print(f"Splitted csv is longer/shorter than it should be! {len(studentData)}")
-        #     quit()
-
         if ":" not in studentData[0]:
             # if there is no timestamp in this line or this is the header line
             continue
+
+        # calcualte a number from the time the form is filled
+        # based on the time elapsed from the start of time (1970 something)
+        parsedDateTimeObject = datetime.datetime.strptime(studentData[FORM_FILLED_DATE], "%m/%d/%Y %H:%M:%S")
+        elapsedTimeFromTheStartOfTime = parsedDateTimeObject.timestamp() - 1654224090
 
         # print(studentData)
         print("Processing: " + studentData[REG_NO] + " " + studentData[NAME_WITH_INITIALS])
 
         nameConverted = studentData[NAME_WITH_INITIALS].replace(" ", "").replace(".", "")
         permalink = f"/students/postgraduate/{nameConverted}"
-
-        # interests
-        # interests = ",".join(studentData[INTERESTS].split(";"))
-
-        # location
-        # location = ",".join(studentData[LOCATION].split(";"))
 
         # image
         image_path = f"images/students/postgraduate/{nameConverted}.jpg"
@@ -77,6 +70,7 @@ if __name__ == "__main__":
 layout: postgraduateDetails
 permalink: "{permalink}"
 title: {studentData[NAME_WITH_INITIALS]}
+index_in_card_list: {int(elapsedTimeFromTheStartOfTime)}
 
 reg_no: {studentData[REG_NO]}
 name_with_initials: {studentData[NAME_WITH_INITIALS]}
@@ -96,29 +90,4 @@ image_url: "{image_path}"
         htmlFile.write(outputString)
         htmlFile.close()
 
-        # # update json if below E14
-        # if int(batch[0:2]) < 14:
-        #     print("Updating JSON in _data folder")
-
-        #     # select student from json file
-        #     jsonPath = f"../_data/stud/e{batch.lower()}.json"
-        #     dataInJSON = json.load(open(jsonPath))
-        #     thisStudent = dataInJSON[studentData[REG_NO].upper()]
-
-        #     # change data
-        #     thisStudent["page_url"] = f"/students/e{batch.lower()}/{regNo}/"
-        #     thisStudent["name_with_initials"] = f"{studentData[NAME_WITH_INITIALS]}"
-        #     if isImageDownloaded:
-        #         thisStudent["image_url"] = image_path
-
-        #     # write data back into json file
-        #     jsonFile = open(jsonPath, "w")
-        #     jsonFile.write(json.dumps(dataInJSON, indent=4))
-
-        print("-------------")
-
-# print("\n\n\n\n")
-# print("Updating Student page titles")
-# student_profile_page_titles.run()
-# print("Resizing Images")
-# resize_student_images.run()
+print("Done")
