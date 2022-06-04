6f1a42117fa7a8598f8fa3bd8201c6d821bce20f
NuwanJ <nuwanjaliyagoda@gmail.com>
2022-06-04 20:47:16 +0530

    Postgraduate page updates

--- python_scripts/update_postgrad_from_google_form.py
+++ python_scripts/update_postgrad_from_google_form.py
@@ -4,12 +4,12 @@
 
 # Author: E/18/098 Ishan Fernando - e18098@eng.pdn.ac.lk
 
-
 import requests
 import os
 import gdown  # pip install gdown
 import json  # to edit _data/exx.json
 import student_profile_page_titles
+import shutil
 import resize_student_images
 
 googleFromCSV_link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR6ntuDUUuzQS84Am7NMcCc6LCTYKmUfMbVhp4dvy1AXWoVxNrjWfeQntWg5cAfLsFvb4WASQRp-erT/pub?output=csv"
@@ -27,16 +27,18 @@ YEAR_OF_COMPLETION = 12
 PROFILE_PIC_LINK = 15
 WEBSITE_URL = 16
 
-
 i = 0
 if __name__ == "__main__":
+    # Delete old entries
+    try:
+        shutil.rmtree('../images/students/postgraduate/')
+        shutil.rmtree('../pages/postgraduate/students/')
+    except OSError as e:
+        print("Error: %s : %s" % ('Directory remove failed', e.strerror))
+
+
     for eachLine in googleFromCSV:
         studentData = eachLine.replace('\r', '').split(",")
-
-        # URL to website is optional therefore it may not be included in this line (last comma is not there)
-        # if len(studentData) != 16 :
-        #     print(f"Splitted csv is longer/shorter than it should be! {len(studentData)}")
-        #     quit()
 
         if ":" not in studentData[0]:
             # if there is no timestamp in this line or this is the header line
@@ -48,23 +50,14 @@ if __name__ == "__main__":
         nameConverted = studentData[NAME_WITH_INITIALS].replace(" ", "").replace(".", "")
         permalink = f"/students/postgraduate/{nameConverted}"
 
-        # interests
-        # interests = ",".join(studentData[INTERESTS].split(";"))
-
-        # location
-        # location = ",".join(studentData[LOCATION].split(";"))
-
         # image
         image_path = f"images/students/postgraduate/{nameConverted}.jpg"
         isImageDownloaded = False
         if studentData[PROFILE_PIC_LINK] != "" and len(studentData[PROFILE_PIC_LINK]) > 1:
             print(f"Downloading image to {image_path}")
             isImageDownloaded = True
-            # print(len(studentData[URL_IMAGE]))
             gdown.download("https://drive.google.com/uc?id=" +
                            studentData[PROFILE_PIC_LINK].split("=")[1].strip(), "../"+image_path, quiet=True)
-            # os.system(
-            #     f"wget https://drive.google.com/uc?id={studentData[URL_IMAGE].split('=')[1].strip()} -O ../{image_path}")
         else:
             print("Image not specified")
 
@@ -90,35 +83,10 @@ image_url: "{image_path}"
 ---"""
 
         # write to html file
-        file_url = "../"+f"pages/students/postgraudate/{nameConverted}.html"
+        file_url = "../"+f"pages/postgraduate/students/{nameConverted}.html"
         os.makedirs(os.path.dirname(file_url), exist_ok=True)
         htmlFile = open(file_url, "w")
         htmlFile.write(outputString)
         htmlFile.close()
 
-        # # update json if below E14
-        # if int(batch[0:2]) < 14:
-        #     print("Updating JSON in _data folder")
-
-        #     # select student from json file
-        #     jsonPath = f"../_data/stud/e{batch.lower()}.json"
-        #     dataInJSON = json.load(open(jsonPath))
-        #     thisStudent = dataInJSON[studentData[REG_NO].upper()]
-
-        #     # change data
-        #     thisStudent["page_url"] = f"/students/e{batch.lower()}/{regNo}/"
-        #     thisStudent["name_with_initials"] = f"{studentData[NAME_WITH_INITIALS]}"
-        #     if isImageDownloaded:
-        #         thisStudent["image_url"] = image_path
-
-        #     # write data back into json file
-        #     jsonFile = open(jsonPath, "w")
-        #     jsonFile.write(json.dumps(dataInJSON, indent=4))
-
         print("-------------")
-
-# print("\n\n\n\n")
-# print("Updating Student page titles")
-# student_profile_page_titles.run()
-# print("Resizing Images")
-# resize_student_images.run()
