25aee338deae8ae16449c7eb9aa51ba7e9c92bbb
IshanFernando <e18098@eng.pdn.ac.lk>
2022-10-19 22:33:38 +0530

    Print debug data in GH action

--- python_scripts/update_profile_from_google_form.py
+++ python_scripts/update_profile_from_google_form.py
@@ -158,6 +158,7 @@ image_url: {image_path}
             # print(fileLastEditedDateSTR)
             fileLastEditedDate = datetime.strptime(fileLastEditedDateSTR, "%Y-%m-%d %H:%M:%S %z")
             googleFormFilledDate = datetime.strptime(studentData[TIMESTAMP] + " +05:30", "%m/%d/%Y %H:%M:%S %z")
+            print(f"fileLastEditedDate: {fileLastEditedDate}, googleFormFilledDate: {googleFormFilledDate}, Difference: {(fileLastEditedDate - googleFormFilledDate).total_seconds()}")
 
             if (fileLastEditedDate - googleFormFilledDate).total_seconds() > 0:
                 print("File was updated after the google form was filled. Skipping...")
