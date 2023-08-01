7fe92d9eb071373c100925c570e20ed19e26c1ca
Nuwan Jaliyagoda <nuwan.jaliyagoda@rhino-partners.com>
2023-08-01 10:25:25 +0530

    First Round resizing

--- python_scripts/resize_student_images.py
+++ python_scripts/resize_student_images.py
@@ -5,6 +5,32 @@ import os
 from PIL import Image  # pip install pillow
 
 
+def crop_to_square(imagePath):
+    image = Image.open(imagePath)
+    width, height = image.size
+    size = os.path.getsize(imagePath) / 1024
+    if size > 25 or (width != height):
+        # Resize
+        new_width = int(width * 300 / width)
+        new_height = int(height * 300 / width)
+        resized_image = image.resize((new_width, new_height))
+
+        # Crop
+        size = min(new_width, new_height)
+        left = (width - size) // 2
+        upper = (height - size) // 2
+        right = left + size
+        lower = upper + size
+
+        cropped_image = resized_image.crop((left, upper, right, lower))
+
+        if image.size != cropped_image.size:
+            cropped_image.save(imagePath)
+            print("\t {} Resized and cropped".format(imagePath))
+        else:
+            print("\t {} No resize required".format(imagePath))
+
+
 def run():
     imagesPath = "../images/students"
     directory_list = os.listdir(imagesPath)
@@ -12,25 +38,17 @@ def run():
     for eachFolder in directory_list:
         if "." in eachFolder:
             continue  # skip if not folder
-        for eachImage in os.listdir(imagesPath+f"/{eachFolder}"):
-            if (eachImage == "e16172.jpg" or eachImage == "e18366.jpg"):
-                continue  # this was a png so the resizing doesnt work properly
-            try:
-                imagePath = imagesPath+f"/{eachFolder}/{eachImage}"
-                image = Image.open(imagePath)
-                width, height = image.size
-                size = os.path.getsize(imagePath)/1024
-                if size > 25:
-                    print(f"{imagePath}, width = {width:5}, height = {height:5}, size = {size:4.4}kb")
-                    resizeValue = 300/width
-                    new_image = image.resize((int(width*resizeValue), int(height*resizeValue)))
 
-                    if(not (int(width*resizeValue) == width and int(height*resizeValue) == height)):
-                        new_image.save(imagePath)
-                    else:
-                        print("No resize required")
-            except:
-                print(f"Failed to resize image {imagePath}")
+        for eachImage in os.listdir(imagesPath + f"/{eachFolder}"):
+            if eachImage == "e16172.jpg" or eachImage == "e18366.jpg":
+                continue  # this was a png so the resizing doesnt work properly
+
+            try:
+                imagePath = imagesPath + f"/{eachFolder}/{eachImage}"
+                crop_to_square(imagePath)
+
+            except Exception as e:
+                print(f"\t Failed to resize image {imagePath}, {e}")
 
 
 if __name__ == "__main__":
