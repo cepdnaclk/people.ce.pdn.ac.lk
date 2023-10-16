24cc57352f9befa51a225621443ad011275ba082
Nuwan Jaliyagoda <nuwan.jaliyagoda@rhino-partners.com>
2023-10-16 12:26:16 +0530

    PG Student updates

--- python_scripts/update_postgrad_from_google_form.py
+++ python_scripts/update_postgrad_from_google_form.py
@@ -13,8 +13,9 @@ import shutil
 import json
 
 googleFromCSV_link = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR6ntuDUUuzQS84Am7NMcCc6LCTYKmUfMbVhp4dvy1AXWoVxNrjWfeQntWg5cAfLsFvb4WASQRp-erT/pub?output=csv"
-googleFromCSV = requests.get(googleFromCSV_link, headers={
-                             'Cache-Control': 'no-cache'}).text.split("\n")
+googleFromCSV = requests.get(
+    googleFromCSV_link, headers={"Cache-Control": "no-cache"}
+).text.split("\n")
 
 # Index of the CSV parameters
 FORM_FILLED_DATE = 0
@@ -33,12 +34,12 @@ i = 0
 if __name__ == "__main__":
     try:
         # shutil.rmtree('../images/students/postgraduate/')
-        shutil.rmtree('../pages/postgraudate/students')
+        shutil.rmtree("../pages/postgraudate/students")
     except OSError as e:
-        print("Error: %s : %s" % ('Directory remove failed', e.strerror))
+        print("Error: %s : %s" % ("Directory remove failed", e.strerror))
 
     for eachLine in googleFromCSV:
-        studentData = eachLine.replace('\r', '').split(",")
+        studentData = eachLine.replace("\r", "").split(",")
 
         if ":" not in studentData[0]:
             # if there is no timestamp in this line or this is the header line
@@ -46,16 +47,22 @@ if __name__ == "__main__":
 
         # calcualte a number from the time the form is filled
         # based on the time elapsed from the start of time (1970 something)
-        parsedDateTimeObject = datetime.datetime.strptime(studentData[FORM_FILLED_DATE], "%m/%d/%Y %H:%M:%S")
+        parsedDateTimeObject = datetime.datetime.strptime(
+            studentData[FORM_FILLED_DATE], "%m/%d/%Y %H:%M:%S"
+        )
         elapsedTimeFromTheStartOfTime = parsedDateTimeObject.timestamp() - 1654224090
         print(int(elapsedTimeFromTheStartOfTime))
 
         # print(studentData)
-        print("Processing: " + studentData[REG_NO] + " " + studentData[NAME_WITH_INITIALS])
+        print(
+            "Processing: " + studentData[REG_NO] + " " + studentData[NAME_WITH_INITIALS]
+        )
 
         print(json.dumps(studentData, indent=4))
 
-        nameConverted = studentData[NAME_WITH_INITIALS].replace(" ", "").replace(".", "")
+        nameConverted = (
+            studentData[NAME_WITH_INITIALS].replace(" ", "").replace(".", "")
+        )
         permalink = f"/students/postgraduate/{nameConverted}/"
 
         # image
@@ -63,24 +70,33 @@ if __name__ == "__main__":
         os.makedirs(os.path.dirname(image_path), exist_ok=True)
 
         isImageDownloaded = False
-        if studentData[PROFILE_PIC_LINK] != "" and len(studentData[PROFILE_PIC_LINK]) > 1:
-            print(f"Downloading image to {image_path}")
-            isImageDownloaded = True
-            # print(len(studentData[URL_IMAGE]))
-            gdown.download("https://drive.google.com/uc?id=" +
-                           studentData[PROFILE_PIC_LINK].split("=")[1].strip(), "../"+image_path, quiet=True)
-
+        if (
+            studentData[PROFILE_PIC_LINK] != ""
+            and len(studentData[PROFILE_PIC_LINK]) > 1
+        ):
+            if not os.path.exists("../" + image_path):
+                print(f"Downloading image to {image_path}")
+                isImageDownloaded = True
+                # print(studentData[PROFILE_PIC_LINK])
+                gdown.download(
+                    "https://drive.google.com/uc?id="
+                    + studentData[PROFILE_PIC_LINK].split("=")[1].strip(),
+                    "../" + image_path,
+                    quiet=True,
+                )
+            else:
+                print("Profile pic is already there")
         else:
             image_path = "images/students/default.jpg"
             print("Image not specified")
 
         # add # to all empty fields
-        for i in range(0, WEBSITE_URL+1):
+        for i in range(0, WEBSITE_URL + 1):
             if studentData[i] == "":
                 studentData[i] = "#"
 
         reg_number = studentData[REG_NO]
-        if(reg_number.lower() == "provisional"):
+        if reg_number.lower() == "provisional":
             # Some will not type it in the correct case
             reg_number = "Provisional"
 
@@ -102,7 +118,7 @@ image_url: "{image_path}"
 ---"""
 
         # write to html file
-        file_url = "../"+f"pages/postgraudate/students/{nameConverted}.html"
+        file_url = "../" + f"pages/postgraudate/students/{nameConverted}.html"
         os.makedirs(os.path.dirname(file_url), exist_ok=True)
         htmlFile = open(file_url, "w")
         print(f"Writing to {file_url}")
