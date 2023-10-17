ca3112caecf599a8a5724a603d2b2cd7579b6e93
IshanFernando <73381996+ishanfdo18098@users.noreply.github.com>
2023-10-17 10:39:27 +0530

    Postgrad publications (#353)
    * Add publication to postgrad #352
    
    * Add postgrad students to API
    
    * Use email as the key #352
    
    * Manual update of the PG research
    
    ---------
    
    Co-authored-by: Nuwan Jaliyagoda <nuwan.jaliyagoda@rhino-partners.com>

--- python_scripts/update_publications.py
+++ python_scripts/update_publications.py
@@ -12,27 +12,27 @@ try:
     url = "https://api.ce.pdn.ac.lk/publications/v1/filter/students/index.json"
     r = requests.get(url)
 
-    if r.status_code==200:
+    if r.status_code == 200:
         # it is available
         stud_pub = json.loads(r.text)
 
         filename = "../_data/student_publications.json"
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, "w") as f:
-            f.write(json.dumps(stud_pub, indent = 4))
+            f.write(json.dumps(stud_pub, indent=4))
         print("Student: Success")
     else:
         print("Student: Failed")
 
 except:
-    print('parse failed; ' +  url)
+    print("parse failed; " + url)
 
 # Get staff publications
 try:
     url = "https://api.ce.pdn.ac.lk/publications/v1/filter/staff/index.json"
     r = requests.get(url)
 
-    if r.status_code==200:
+    if r.status_code == 200:
         # it is available
         staff_pub = json.loads(r.text)
         staff_pub_year = {}
@@ -42,35 +42,41 @@ try:
             pub_count = 0
             year_count = 0
 
-            if staff not in staff_pub_year: staff_pub_year[staff]= {
-                "publication_count": 0, "year_count": 0, "publications": {}
-            }
+            if staff not in staff_pub_year:
+                staff_pub_year[staff] = {
+                    "publication_count": 0,
+                    "year_count": 0,
+                    "publications": {},
+                }
 
             for pub in staff_pub[staff]:
                 pub_count += 1
-                year = pub['year']
+                year = pub["year"]
 
-                if year not in staff_pub_year[staff]["publications"]: staff_pub_year[staff]["publications"][year] = []
+                if year not in staff_pub_year[staff]["publications"]:
+                    staff_pub_year[staff]["publications"][year] = []
                 staff_pub_year[staff]["publications"][year].append(pub)
 
-            staff_pub_year[staff]['publication_count'] = pub_count
-            staff_pub_year[staff]['year_count'] = len(staff_pub_year[staff]["publications"])
+            staff_pub_year[staff]["publication_count"] = pub_count
+            staff_pub_year[staff]["year_count"] = len(
+                staff_pub_year[staff]["publications"]
+            )
 
         # Sort the publications by year
         for staff in staff_pub:
-            publications = staff_pub_year[staff]['publications']
+            publications = staff_pub_year[staff]["publications"]
             publications_sorted = {}
             for key in sorted(publications, reverse=True):
                 # print "%s: %s" % (key, mydict[key])
                 publications_sorted[key] = publications[key]
 
-            staff_pub_year[staff]['publications'] = publications_sorted
+            staff_pub_year[staff]["publications"] = publications_sorted
 
         # Write into the file
         filename = "../_data/staff_publications.json"
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, "w") as f:
-            f.write(json.dumps(staff_pub_year, indent = 4))
+            f.write(json.dumps(staff_pub_year, indent=4))
 
         print("Staff: Success")
 
@@ -78,4 +84,6 @@ try:
         print("Staff: Failed")
 
 except:
-    print('parse failed; ' +  url)
+    print("parse failed; " + url)
+
+# PG Students will be updated and managed manually
