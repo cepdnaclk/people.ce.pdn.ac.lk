4710b5a1c7f7c4359f0cc4db6c6171e56a933e03
Nuwan Jaliyagoda <nuwan.jaliyagoda@rhino-partners.com>
2024-02-01 22:16:08 +0530

    Address the issue #374

--- python_scripts/update_projects.py
+++ python_scripts/update_projects.py
@@ -11,54 +11,58 @@ try:
     url = "https://api.ce.pdn.ac.lk/projects/v1/filter/students/index.json"
     r = requests.get(url)
 
-    if r.status_code==200:
+    if r.status_code == 200:
         # it is available
         stud_projects = json.loads(r.text)
         filename = "../_data/student_projects.json"
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, "w") as f:
-            f.write(json.dumps(stud_projects, indent = 4))
+            f.write(json.dumps(stud_projects, indent=4))
 
         print("Students: Success")
     else:
         print("Students: Failed")
 
 except:
-    print('parse failed; ' +  url)
+    print("parse failed; " + url)
 
 try:
     url = "https://api.ce.pdn.ac.lk/projects/v1/filter/staff/index.json"
     r = requests.get(url)
 
-    if r.status_code==200:
+    if r.status_code == 200:
         # it is available, group by batch
         staff_projects = json.loads(r.text)
         grouped_proj = {}
 
         for s in staff_projects:
             if s not in grouped_proj:
-                grouped_proj[s] = { 'count': 0, 'projects': {} }
+                grouped_proj[s] = {"count": 0, "projects": {}}
 
             for p in staff_projects[s]:
-                batch = p['project_url'].split('/')[4]
-                cat = p['project_url'].split('/')[3]
-                
-                if cat == "4yp":
-                    grouped_proj[s]['count'] = grouped_proj[s]['count'] + 1 
+                batch = p["project_url"].split("/")[4]
+                cat = p["project_url"].split("/")[3]
 
-                    if batch in grouped_proj[s]['projects']:
-                        grouped_proj[s]['projects'][batch].append(p)
+                if cat == "4yp":
+                    grouped_proj[s]["count"] = grouped_proj[s]["count"] + 1
+
+                    if batch in grouped_proj[s]["projects"]:
+                        grouped_proj[s]["projects"][batch].append(p)
                     else:
-                        grouped_proj[s]['projects'][batch] = [p]
-                    
+                        grouped_proj[s]["projects"][batch] = [p]
+
+            grouped_proj[s]["projects"] = dict(
+                sorted(grouped_proj[s]["projects"].items(), reverse=True)
+            )
+
         filename = "../_data/staff_projects.json"
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, "w") as f:
-            f.write(json.dumps(grouped_proj, indent = 4))
+            f.write(json.dumps(grouped_proj, indent=4))
 
         print("Staff: Success")
     else:
         print("Staff: Failed")
 
 except:
-    print('parse failed; ' +  url)
+    print("parse failed; " + url)
