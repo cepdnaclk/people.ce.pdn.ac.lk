4c9117a5d8bd49d6eed163494ce49ec81e365af8
Thisen2002 <e21231@eng.pdn.ac.lk>
2025-03-29 17:10:57 +0530

    Initial commit

---
+++ python_scripts/add_new_student_batch/jpg rename/Rename.py
@@ -0,0 +1,25 @@
+import os
+
+# Set the directory containing the .jpg files
+directory = "C:\\Users\\prama\\Downloads\\people.ce.pdn.ac.lk\\images\\students\\e22\\ori"  # Replace with your directory path
+directory_1 = "C:\\Users\\prama\\Downloads\\people.ce.pdn.ac.lk\\images\\students\\e22\\rename"
+
+# Get all files in the directory
+files = os.listdir(directory)
+
+# Filter for .jpg files
+jpg_files = [f for f in files if f.lower().endswith('.jpg')]
+
+# Rename files
+for index, file in enumerate(jpg_files):
+    # Define the new file name, e.g., "image_1.jpg"
+    new_name = f"e22{file}"
+    
+    # Get the full file paths
+    old_path = os.path.join(directory, file)
+    new_path = os.path.join(directory_1, new_name)
+    
+    # Rename the file
+    os.rename(old_path, new_path)
+
+    print(f"Renamed {file} to {new_name}")
